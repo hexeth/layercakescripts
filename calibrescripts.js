5d9b5e2606799aea09de45aafc3d9f6c5979dce6
hexeth <7627137+hexeth@users.noreply.github.com>
2018-07-09 11:37:32 -0700

    Add class to iframe body in reader view

--- calibrescripts.js
+++ calibrescripts.js
@@ -1,6 +1,6 @@
 //Split path name to array and remove blanks
-var urlWithBlanks = window.location.pathname.split("/");
-var url = urlWithBlanks.filter(function(v){return v!==''});
+var urlWithBlanks = window.location.pathname.split( "/" );
+var url = urlWithBlanks.filter( function(v){return v!==''} );
 
 //If array only has one item, add home class
 //Else If path ends in number, go two items back in array to get path name and add path as class to body
@@ -13,6 +13,9 @@ if ( url.length == 1 ) {
 			 blur = url[url.length-2] ;
 			 $( "body" ).addClass( blur );
 			 }
+			 if ( ( '.epub' ).length > 0 ); {
+			 $('iframe[id^=epub').contents().find('body').addClass('epub-frame');
+			}
 	else {
 			blur = url[url.length-1];
 			$( "body" ).addClass( blur );
@@ -24,16 +27,16 @@ firstDownload = $( 'a[id^=btnGroupDrop]' ).first();
 lastDownload = $( 'a[id^=btnGroupDrop]' ).last();
 downloadItems = $( 'a[id^=btnGroupDrop]' );
 
-if ($( 'a[id^=btnGroupDrop]' ).length == 2) {
+if ($( 'a[id^=btnGroupDrop]' ).length == 2 ) {
 
-	downloadItems.find('span').remove()
+	downloadItems.find( 'span' ).remove()
 	downloadItems.removeClass( 'btn btn-primary' );
 	downloadItems.removeAttr( 'role' );
 
 	$( '<button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-download"></span>Download :<span class="caret"></span></button><ul class="dropdown-menu leramslist aria-labelledby="btnGroupDrop1"><li></li><li></li></ul>' ).insertBefore( firstDownload );
 
-	$( firstDownload ).prependTo(".leramslist li:first-child");
-	$( lastDownload ).prependTo(".leramslist li:last-child");
+	$( firstDownload ).prependTo( ".leramslist li:first-child" );
+	$( lastDownload ).prependTo( ".leramslist li:last-child" );
 }
 
 //remove the popup modals
