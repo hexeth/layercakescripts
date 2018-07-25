d9c61ef0cd29f69e045a309c7dbc537d72173a77
hexeth <7627137+hexeth@users.noreply.github.com>
2018-07-25 10:31:38 -0700

    done

--- calibrescripts.js
+++ calibrescripts.js
@@ -2,35 +2,19 @@
 var urlWithBlanks = window.location.pathname.split( "/" );
 var url = urlWithBlanks.filter( function(v){return v!==''} );
 
-//If array only has one item, add home class
-//Else If last path is not a number, go one item back in path, add path name as class to body
-if ( url.length == 1 ) {
-	$( "body" ).addClass( "home" );
-}
-else {  
-	$( "body").addClass( url[1]);
+
+if ( jQuery.inArray( 'epub', url ) != -1 ) {
+        $( "body" ).addClass( url[3] );
 }
 
-//If there are exactly 2 types of ebooks for a single book title, format to list
-downloads = $( 'a[id^=btnGroupDrop]' ).get();
-
-if ( $( downloads ).length > 1 ) {
-	$( '<button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-download"></span>Download :<span class="caret"></span></button><ul class="dropdown-menu leramslist aria-labelledby="btnGroupDrop1"></ul>' ).insertBefore( downloads[downloads.length-1] );
-	$( downloads ).detach();
-	$.each(downloads, function (i,val) {
-		$("<li>" + downloads[i].outerHTML + "</li>").appendTo( ".leramslist" );
-	});
-	$( ".leramslist" ).find( 'span' ).remove();
-	$( ".leramslist a" ).removeClass( 'btn btn-primary' ).removeAttr( 'role' );
-}
+//add class to iframe body after it loads.
+$('body.read').on('DOMNodeInserted', 'iframe', function(e) {
+        setTimeout(function() {
+                $("iframe").contents().find("body").addClass( 'read-frame' );
+        }, 1000);
+});
 
 //remove the popup modals
-if (!$( "body" ).hasClass('admin')) { 
+if (!$( "body" ).hasClass('admin')) {
 $(' a:not(.dropdown-toggle) ').removeAttr( 'data-toggle', 'data-target', 'data-remote' );
 }
-
-$('body.read').on('DOMNodeInserted', 'iframe', function(e) {
-	setTimeout(function() {
-		$("iframe").contents().find("body").addClass( 'read-frame' );
-	}, 750);
-});
